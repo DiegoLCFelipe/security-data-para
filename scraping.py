37b3728d308a96bfbe62dd8b9221cee6f9846104
diego <diego.felipe095@gmail.com>
2022-02-14 08:57:52 -0300

    Mudança da inserção manual dos links para a inserção automática

--- scraping.py
+++ scraping.py
@@ -1,50 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 from WebTable import WebTable
 from FormatTable import FormatTable
+from Links import Links
 
-path = 'http://sistemas.segup.pa.gov.br/transparencia/'
-
-dic_tabels = {
-    'tablepress-145': 'homicidios-2021',
-    'tablepress-151': 'latrocinios-2021',
-    'tablepress-155': 'lesao-corporal-seguida-de-morte-2021',
-    'tablepress-153': 'roubo-2021',
-    'tablepress-154': 'trafico-2021',
-    'tablepress-156': 'estupros-2020-3',
-    'tablepress-158': 'furtos-2021',
-    'tablepress-152': 'lesao-corporal-2020-2',
-
-    'tablepress-159': 'homicidios-no-transito-2021',
-    'tablepress-160': 'morte-no-transito-2021',
-    'tablepress-161': 'lesao-no-transito-2021',
-    'tablepress-168': 'morte-decorrente-de-intervencao-policial-2021',
-    'tablepress-169': 'policiais-mortos-em-servico-2021',
-    'tablepress-170': 'policiais-mortos-fora-de-servico-2021',
-    'tablepress-171': 'feminicidio-2021'
-}
+url = 'http://sistemas.segup.pa.gov.br/transparencia/estatisticas-2021/'
 
 lst_dados = []
 
-for key, value in zip(dic_tabels.keys(), dic_tabels.values()):
-    Table = WebTable(path + value)
-    header = Table.table_header()
-    content = Table.table_content()
+links = Links(url)
+dict_tables = links.tag_split()
 
-    id_vars = header[0:1]
+print(dict_tables)
 
-    value_vars = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET']
+for title, link in zip(dict_tables.keys(), dict_tables.values()):
+    if title != 'Ocorrências':
+        Table = WebTable(link)
+        header = Table.table_header()
+        content = Table.table_content()
 
-    dados = pd.DataFrame(columns=header[0:12], data=content)
+        id_vars = header[0:2]
+        value_vars = header[2:12]
+    else:
+        continue
+
+    dados = pd.DataFrame(columns=header[0:12], data=content[0:12])
+
     table_formated = FormatTable(dados)
     table_formated.wide_to_long_format(id_vars, value_vars, 'MÊS', 'N_OCORRENCIAS')
     table_formated.repeat_record('N_OCORRENCIAS')
-    table_formated.create_columns(['CRIME', 'ANO'], [value[:-5], '2021'])
+    table_formated.create_columns(['CRIME', 'ANO'], [title, url[-1:-5]])
     lst_dados.append(table_formated.dados)
 
 dados = pd.concat(lst_dados, ignore_index=True)
-dados.to_csv('ocorrencias.csv', index=False)
+# # dados.to_csv('ocorrencias.csv', index=False)
 print(dados)
-
-
