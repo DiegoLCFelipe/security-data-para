3352998fdb86bfb91a15558a263d9bb080e5c329
diego <diego.felipe095@gmail.com>
2022-02-19 13:26:58 -0300

    Refactor code and add table handler features

--- Helper/LXMLHandler.py
+++ Helper/LXMLHandler.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 
 class LXMLHandler:
 
-    def __init__(self, web_page_lxml_code):
-        self.__web_page_lxml_code = web_page_lxml_code
+    def __init__(self, lxml_code):
+        self.__lxml_code = lxml_code
 
-    def find_urls_lines(self):
-        return self.__web_page_lxml_code.find_all(self.tag_filter)
+    def find_url_tags(self):
+        return self.__lxml_code.find_all(self.url_filter)
 
     @staticmethod
-    def tag_filter(tag):
+    def url_filter(tag):
         return tag.has_attr('href') and \
                tag.has_attr('title') and \
                tag.has_attr('class') and not \
@@ -19,25 +19,51 @@ class LXMLHandler:
                    tag.has_attr('property')
 
     @staticmethod
-    def find_url_start(url_line):
+    def _find_url_start(url_line):
         return url_line.find('href') + 6
 
     @staticmethod
-    def find_url_end(url_line):
+    def _find_url_end(url_line):
         return url_line.find('title') - 2
 
     @staticmethod
-    def find_title_start(url_line):
+    def _find_title_start(url_line):
         return url_line.find('title') + 7
 
     @staticmethod
-    def find_title_end(url_line):
+    def _find_title_end(url_line):
         return url_line.find('>') - 6
 
-    def find_tag_link(self, tag):
-        url = self.find_urls_lines()
-        return str(url[tag])[self.find_url_start(str(url[tag])):self.find_url_end(str(url[tag]))]
+    def get_urls(self):
+        lst_links_of_page = []
+        lst_titles_of_page = []
+        for tag in self.find_url_tags():
+            lst_links_of_page.append(str(tag)[self._find_url_start(str(tag)):self._find_url_end(str(tag))])
+            lst_titles_of_page.append(
+                str(tag)[self._find_title_start(str(tag)):self._find_title_end(str(tag))])
 
-    def find_tag_title(self, tag):
-        url = self.find_urls_lines()
-        return str(url[tag])[self.find_title_start(str(url[tag])):self.find_title_end(str(url[tag]))]
+        return lst_titles_of_page, lst_links_of_page
+
+    def find_table(self):
+        return self.__lxml_code.find('table')
+
+    def table_content(self):
+        lst_row_data = []
+        for trs in self.find_table().find_all('tr')[1:]:
+            row_data = trs.find_all('td')
+            row = [i.text.replace(",", "") for i in row_data]
+
+            lst_row_data.append(row)
+        return lst_row_data
+
+    def table_header(self):
+        table_content = self.table_content()
+        lst_header = []
+        for i in self.find_table().find_all('th'):
+            title = i.text
+            lst_header.append(title)
+
+        if len(lst_header) > len(table_content[0]):
+            return lst_header[0:len(table_content[0])]
+
+        return lst_header
