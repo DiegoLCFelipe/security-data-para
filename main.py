3d79f5f0a3b19729444e3b5fc27af577cc7862bc
Diogo Felipe <dlcfelipp@gmail.com>
2022-02-22 15:05:47 -0300

    feat(PSD-01): add log handler

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from Request import Requester
 from Helper.AdpterHTMLToLXML import AdapterHTMLToLXML
 from Helper.LXMLHandler import LXMLHandler
 from Helper.FileHandler import FileHandler
+from Helper.LogHandler import LogHandler
 from bs4 import BeautifulSoup
 
+logHandler = LogHandler()
 
 def should_get_data_from_web(path):
     data_directory_path = path
-    print(f"Directory {path} is not empty \n")
+    logHandler.log_error_timestamp(f"Directory {path} is not empty \n")
     if not os.listdir(data_directory_path):
         return True
     return False
@@ -17,7 +19,7 @@ def should_get_data_from_web(path):
 def should_by_pass_file(file_name, exception_words):
     for words in exception_words:
         if words in file_name:
-            print(f'By Passing {file_name}')
+            logHandler.log_info_timestamp(f'By Passing {file_name}')
             return True
         return False
 
@@ -29,29 +31,27 @@ lst_data_frame = []
 
 # Web Scrapping
 if should_get_data_from_web(DATA_PATH):
-    print(f'{DATA_PATH} is empty: Performing web scrapping.. {URL_BASE} \n')
+    logHandler.log_info_timestamp(f'{DATA_PATH} is empty: Performing web scrapping.. {URL_BASE} \n')
     for year in range(2010, 2021)[0:1]:
-        portal_transparencia_html = Requester(URL_BASE + str(year)).page_html_code()
+        portal_transparencia_html = Requester(URL_BASE + str(year), logHandler).page_html_code()
         portal_transparencia_html_to_lxml = AdapterHTMLToLXML(portal_transparencia_html).parse_xml()
         lxml_tags_with_urls = LXMLHandler(portal_transparencia_html_to_lxml)
         titles, urls = lxml_tags_with_urls.get_urls()
         for title, url in zip(titles, urls):
-            FileHandler(url).save_html_code_locally(DATA_PATH +  str(title) + '_' + str(year) + '.html')
+            FileHandler(url, logHandler).save_html_code_locally(DATA_PATH +  str(title) + '_' + str(year) + '.html')
 
 # Data Mananger
 for file_name in os.listdir(DATA_PATH):
     if should_by_pass_file(file_name, IGNORED_FILES_NAMES):
         continue
 
-    html_data_saved_locally = FileHandler(DATA_PATH + file_name).open_file()
+    html_data_saved_locally = FileHandler(DATA_PATH + file_name, logHandler).open_file()
     lxml_from_data_saved_locally = AdapterHTMLToLXML(html_data_saved_locally).parse_xml()
-    print(file_name)
     table_content_from_html_local_file = LXMLHandler(lxml_from_data_saved_locally).table_content()
     table_header_from_html_local_file = LXMLHandler(lxml_from_data_saved_locally).table_header()
 
     data_table = pd.DataFrame(columns=table_header_from_html_local_file, data=table_content_from_html_local_file)
 
-    print(data_table)
     # Remove columns 'Total'
 #     data_frame.drop(data_frame.columns[-1], axis=1, inplace=True)
 #
