49741422082f39dc2e21870e8eeb337d4466c3f0
diego <diego.felipe095@gmail.com>
2022-02-26 16:29:09 -0300

    refactor: remove FormatTableStrategy.py and IterfaceTableStrategy.py from project

--- FormatTableStrategies/FormatTableStrategy.py
+++
@@ -1,7 +0,0 @@
-from FormatTableStrategies.IterfaceTableStrategy import InterfaceTableFormatStrategy
-
-
-class FormatTableStrategy(InterfaceTableFormatStrategy):
-
-    def formatLine(self, table_line):
-        return table_line.replace(",", "")
--- FormatTableStrategies/IterfaceTableStrategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class InterfaceTableFormatStrategy(ABC):
-
-    @abstractmethod
-    def formatLine(self, table_line):
-        pass
