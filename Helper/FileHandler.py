9e50ec35a73d6753c74ce6e9e867481cf59744ce
diego <diego.felipe095@gmail.com>
2022-02-26 16:22:29 -0300

    refactor (FileHandler.py): remove log info from "save_html_code_locally" method.

--- Helper/FileHandler.py
+++ Helper/FileHandler.py
@@ -11,5 +11,4 @@ class FileHandler:
         return open(self._path)
 
     def save_html_code_locally(self, path_local):
-        self.logHandler.log_info_timestamp(f'Saving locally: {self._path}')
         urllib.request.urlretrieve(self._path, path_local)
