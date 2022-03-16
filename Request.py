bdff14897f2ce4155a597f3b1a4a627f564bede4
DiegoLCFelipe <diego.felipe095@gmail.com>
2022-03-16 15:32:49 -0300

    refactor: code simplification

--- Request.py
+++ Request.py
@@ -1,4 +1,9 @@
-import requests
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from Helper.LogHandler import LogHandler
+
+logHandler = LogHandler()
 
 
 class Requester:
@@ -6,18 +11,12 @@ class Requester:
     def __init__(self, url, logHandler):
         self.logHandler = logHandler
         self.__url = url
-        self.__web_page = self._request()
 
-    def _request(self):
-        return requests.get(self.__url)
-
-    def _successful_request(self):
-        if self.__web_page.status_code == 200:
-            self.logHandler.log_info_timestamp(f'Successful request on: {str(self.__url)}')
-            return True
-        raise RuntimeError(f'Request Failure on: {str(self.__url)}')
-
-    def page_html_code(self):
-        if self._successful_request():
-            return self.__web_page.text
-
+    def get_html(self):
+        try:
+            return urlopen(self.__url)
+        except HTTPError as error:
+            logHandler.log_error_timestamp(error)
+        except URLError:
+            logHandler.log_error_timestamp('The server could not be found !')
+        logHandler.log_error_timestamp('Successful request')
